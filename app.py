74dbe7852ceb583e9353f19eadc90512d1f44aff
Defesa-MT <149585408+Defesa-MT@users.noreply.github.com>
2024-01-18 09:35:44 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 if __name__ == '__main__':
     app.run(debug=True)
